39a498e114acc1d41432548588306ec00ea10ccd
Psm178102 <paulo@imobiliariapsm.com.br>
2026-05-25 13:48:01 -0300

    v75.33 Sprint 4B Custos Fixos + Repasses NIBO

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 // ═════════════════════════════════════════════════════════════════════════════
 'use strict';
 
-const SW_VERSION = 'v75.32.0-2026-05-25-DRE-NIBO-REALTIME';
+const SW_VERSION = 'v75.33.0-2026-05-25-CUSTOS-REPASSES-NIBO';
 const CACHE_VERSION = 'psm-os-' + SW_VERSION;
 const ASSET_CACHE   = CACHE_VERSION + '-assets';
 
